49c5ad4ceddea851560a7d327445c504d438fd14
AKHIL KUMAR <akhilpvghi@gmail.com>
2023-02-17 06:43:12 -0800

    Create virtual_keyword.cpp

---
+++ virtual_keyword.cpp
@@ -0,0 +1,49 @@
+// to compile in mobaxterm ==>gcc -g virtual_keyword.cpp -o virtual_keyword -lstdc++
+// to run in mobaxterm ===> ./virtual_keyword
+
+
+// ======================================
+// OUTPUT
+// parent_show
+// parent_view
+// parent_show
+// child_view
+
+// ======================================
+#include<map>
+#include<iostream>
+#include<string>
+
+class parent{
+    public:
+    void show(){
+        std::cout<<"parent_show"<<std::endl;
+    }
+    virtual void view(){
+        std::cout<<"parent_view"<<std::endl;
+    }
+};
+
+class child: public parent{
+    void show(){
+        std::cout<<"child_show"<<std::endl;
+    }
+     void view(){
+        std::cout<<"child_view"<<std::endl;
+    }
+};
+
+int main(){
+    parent* p;
+    parent q;
+    child* c;
+    child c1;
+    p=&q;
+
+    p->show();
+    p->view();
+    p=&c1;
+    p->show();
+    p->view();
+    return 0;
+}
