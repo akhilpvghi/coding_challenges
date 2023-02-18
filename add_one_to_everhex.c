24aed2cf4849c5019b42e2c0d7c777f2d7b76a99
AKHIL KUMAR <akhilpvghi@gmail.com>
2023-02-18 03:09:14 -0800

    Create add_one_to_everhex.c

---
+++ add_one_to_everhex.c
@@ -0,0 +1,33 @@
+// COMPILE
+// gcc -g add_one_to_everhex.c -o add_one_to_everhex
+// ./add_one_to_everhex _input_file _output_file
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    FILE *input_file, *output_file;
+    input_file = fopen(argv[1], "rb");
+    output_file = fopen(argv[2], "wb");
+    if (input_file == NULL || output_file == NULL)
+    {
+        printf("Error opening file.\n");
+        return 1;
+    }
+
+    unsigned char byte;
+    while (fread(&byte, 1, 1, input_file) == 1)
+    {
+        if (byte != 0xff)
+        {
+            byte++;
+        }
+        fwrite(&byte, 1, 1, output_file);
+    }
+
+    fclose(input_file);
+    fclose(output_file);
+
+    printf("File processed successfully.\n");
+    return 0;
+}
